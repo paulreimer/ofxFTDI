1145c49ac182ad3f8b5066a8d956a39eff8e1d55
Paul Reimer <paul@p-rimes.net>
2012-11-03 15:43:44 -0700

    Added basic libmpsse wrapper with connect(<same args as mpsse_easy_open>) and send(std::string) API

---
+++ src/ofxMPSSE.cpp
@@ -0,0 +1,47 @@
+#include "ofxMPSSE.h"
+#include <iostream>
+
+//--------------------------------------------------------------
+ofxMPSSE::ofxMPSSE()
+: ftdi(NULL)
+, ftdiRunning(false)
+{}
+
+//--------------------------------------------------------------
+ofxMPSSE::~ofxMPSSE()
+{
+  if (ftdiRunning)
+    mpsse_close(ftdi);
+}
+
+//--------------------------------------------------------------
+bool
+ofxMPSSE::connect(enum modes mode, int freq, int endianess)
+{
+  ftdi = mpsse_easy_open(mode, freq, endianess);
+  if (ftdi != NULL && ftdi->open)
+  {
+    std::cout
+    << "FTDI device '"      << mpsse_get_description(ftdi)
+    << "' initialized at "  << mpsse_get_clock(ftdi) << "Hz"
+    << std::endl;
+
+    ftdiRunning = true;
+  }
+  else {
+    std::cout
+    << "Unable to connect to FTDI device; "
+    << "don't forget to disable (or uninstall) the FTDI official/VCP drivers:"
+    << std::endl
+    << "sudo kextunload /System/Library/Extensions/FTDIUSBSerialDriver.kext"
+    << std::endl;
+  }
+}
+
+//--------------------------------------------------------------
+void
+ofxMPSSE::send(const std::vector<uint8_t>& data)
+{
+  if (ftdiRunning)
+    mpsse_write(ftdi, (char*)data.data(), data.size());
+}
---
+++ src/ofxMPSSE.h
@@ -0,0 +1,21 @@
+#pragma once
+
+extern "C" {
+#include "mpsse.h"
+}
+
+#include <vector>
+
+class ofxMPSSE
+{
+public:
+  ofxMPSSE();
+  ~ofxMPSSE();
+  
+  bool connect(enum modes mode=SPI0, int freq=ONE_MHZ, int endianess=MSB);
+  void send(const std::vector<uint8_t>& data);
+
+private:
+  struct mpsse_context *ftdi;
+  bool ftdiRunning;
+};
