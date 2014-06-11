19c0ac4c21d79d9a20e345e09013ac8bd886685b
Paul Reimer <paul@p-rimes.net>
2014-06-11 11:54:26 -0700

    Added include <stdint.h> for size_t in async.h, zerocopy.h

--- src/async.h
+++ src/async.h
@@ -1,5 +1,6 @@
 #pragma once
 
 #include "mpsse.h"
+#include <stddef.h>
 
 struct ftdi_transfer_control* AsyncWrite(struct mpsse_context *mpsse, uint8_t* buffer, size_t size);
--- src/zerocopy.h
+++ src/zerocopy.h
@@ -1,5 +1,6 @@
 #pragma once
 
 #include "mpsse.h"
+#include <stddef.h>
 
 int ZeroCopyWrite(struct mpsse_context *mpsse, uint8_t* buffer, size_t size);
